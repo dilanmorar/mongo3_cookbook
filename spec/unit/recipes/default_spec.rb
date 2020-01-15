6482fc4afb182ac307cc9e411ef03de18accc7d9
Dilan Morar <DMorar@spartaglobal.com>
2020-01-15 20:41:38 +0000

    mongo version 4 installed in ubuntu 18.04

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -15,15 +15,28 @@ describe 'mongo4_cookbook::default' do
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
-  end
 
-  context 'When all attributes are default, on CentOS 7' do
-    # for a complete list of available platforms and versions see:
-    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'centos', '7'
-
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it 'should install mongodb' do
+      expect(chef_run).to install_package 'mongodb-org'
     end
+
+    it 'enables mongod service' do
+      expect(chef_run).to enable_service 'mongod'
+    end
+
+    it 'start mongod service' do
+      expect(chef_run).to start_service 'mongod'
+    end
+
   end
+
+  # context 'When all attributes are default, on CentOS 7' do
+  #   # for a complete list of available platforms and versions see:
+  #   # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
+  #   platform 'centos', '7'
+  #
+  #   it 'converges successfully' do
+  #     expect { chef_run }.to_not raise_error
+  #   end
+  # end
 end
